c2f612fa71184ce2a6632d7647d883f06426a277
Borja Tornero <borjatornero@gmail.com>
2008-04-05 16:59:21 +0000

    * Resuelto un error que impedía que funcionara correctamente con la    apariencia CLASSIC del correo
    git-svn-id: svn://localhost/tmp/ehun/trunk@8 ae1e012f-e219-41d7-a17a-72f0522a8480

--- chrome/content/ehu-notifier/ehu-notifier.js
+++ chrome/content/ehu-notifier/ehu-notifier.js
@@ -47,64 +47,98 @@ function ehunCheck() {
 		if (httpReq.status == 200) {
 		    htmldoc = new String(httpReq.responseText);
 	        
+			// Get the url of the inbox page
+		    if (htmldoc.search(/cclient/) != -1) {
+				indexA = htmldoc.search(/cclient/);
+				indexB = htmldoc.search(/mail/);
+				subUrl = htmldoc.substring(indexA, indexB);
+				if (subUrl.search(/default/) != -1) {
+					subUrl = htmldoc.substring(indexA, indexB - 44);
+				}
+				else {
+					subUrl = htmldoc.substring(indexA, indexB - 41);
+				}
+		        gUrl = "https://www.ehu.es/cgi-bin/postman/" + subUrl;
+				dump("URL: " + gUrl + "\n");
+		    }	
+
 		    if ( gLang == "spa") {
-			if (htmldoc.search(/No tienes/) != -1) {
-			    num = 0;
-			}    
-			else {
-			    pos = htmldoc.search(/Tienes/);
-			    num = htmldoc.charAt(pos +  7);
-			}
+				search = htmldoc.search(/Correo/);
+				s2 = htmldoc.substring(search);
+				p0 = s2.search(/\(/);
+				p1 = s2.search(/\)/);
+				msg = s2.slice(p0 + 1, p1);
+				msg = msg.replace("&iacute;", "í");
+
+				if (htmldoc.search(/No tienes/) != -1) {
+					num = 0;
+				}    
+				else {
+					pos = htmldoc.search(/Tienes/);
+					num = htmldoc.charAt(pos +  7);
+				}
 		    }
 		    else if (gLang == "eus") {
-			if (htmldoc.search(/Ez/) != -1) {
-			    num = 0;
-			}    
-			else if (htmldoc.search(/bat/) != -1) {
-			    num = 1;
-			}
-			else {
-			    pos = htmldoc.search(/mezu/);
-			    num = htmldoc.charAt(pos - 2);
-			}
+				search = htmldoc.search(/Correo/);
+				s2 = htmldoc.substring(search);
+				p0 = s2.search(/\(/);
+				p1 = s2.search(/\)/);
+				msg = s2.slice(p0 + 1, p1);
+
+				if (htmldoc.search(/Ez/) != -1) {
+					num = 0;
+				}    
+				else if (htmldoc.search(/bat/) != -1) {
+					num = 1;
+				}
+				else {
+					pos = htmldoc.search(/mezu/);
+					num = htmldoc.charAt(pos - 2);
+				}
 		    }
 		    else if (gLang == "eng") {
-			if (htmldoc.search(/No/) != -1) {
-			    num = 0;
-			}    
-			else {
-			    pos = htmldoc.search(/have/);
-			    num = htmldoc.charAt(pos +  5);
-			}
+				search = htmldoc.search(/Correo/);
+				s2 = htmldoc.substring(search);
+				p0 = s2.search(/\(/);
+				p1 = s2.search(/\)/);
+				msg = s2.slice(p0 + 1, p1);
+			
+				if (htmldoc.search(/No/) != -1) {
+					num = 0;
+				}    
+				else {
+					pos = htmldoc.search(/have/);
+					num = htmldoc.charAt(pos +  5);
+				}
 		    }
 		    
 		    if (num == "Y") {
-			num = "Error";
+				num = "Error";
 		    }
 		    
 		    dump("mensajes: "+ num + "\n");
 		    
 		    // Put the number of new emails
 		    document.getElementById('num').value = num;
+			document.getElementById('msg').value = msg;
 
 		    // Change the image if there is new mail
 		    if (num == 0) {
-			document.getElementById('image').src =
-			    "chrome://ehu-notifier/skin/ehun.png";		    
+				document.getElementById('image').src =
+					"chrome://ehu-notifier/skin/ehun.png";		    
 		    }
-		    else {
-		   	document.getElementById('image').src =
-			    "chrome://ehu-notifier/skin/ehun2.png"; 
-		    }	
-		    
-		    // Get the url of the inbox page
-		    if (htmldoc.search(/cclient/) != -1) {
-			indexA = htmldoc.search(/cclient/);
-			indexB = htmldoc.search(/mail/);
-			subUrl = htmldoc.substring(indexA, indexB - 41);
-		        gUrl = "https://www.ehu.es/cgi-bin/postman/" + subUrl;
-			dump("URL: " + gUrl + "\n");
-		    }	
+		    else if (num == "Error") {
+				document.getElementById('image').src =
+					"chrome://ehu-notifier/skin/ehun3.png"; 
+		    }
+			else
+			{
+				document.getElementById('image').src =
+					"chrome://ehu-notifier/skin/ehun2.png"; 
+			}
+			
+			ehunUnread();
+			
 		}	    
 		else {
 	    	    dump("Error loading page\n");
@@ -117,9 +151,59 @@ function ehunCheck() {
 	httpReq.send(gParam);
     }
     
-    //Check every gInterval minutes
+	//Check every gInterval minutes
     intervalMiliSec = gInterval * 60 * 1000;
     setTimeout(ehunCheck, intervalMiliSec);
+}
+
+function ehunUnread(){
+    dump("--- UNREAD ---\n");
+	var sum = "";
+	httpReq = new XMLHttpRequest();
+    httpReq.open('GET', gUrl, true);
+	httpReq.onreadystatechange = function () {
+		if (httpReq.readyState == 4) {	
+			if(httpReq.status == 200) {
+				mailbox = httpReq.responseText;
+				tipbox = document.getElementById("tipbox");
+				dump(tipbox.childNodes.length + "\n");
+				l = tipbox.childNodes.length - 1;
+				for (i = l; i > 0 ; i--) {
+				  tipbox.removeChild(tipbox.childNodes[i]);
+				}
+				while(mailbox.search(/flag_unseen/) > 0) {
+					f = mailbox.search(/flag_unseen/);
+					mailbox2 = mailbox.substring(f + 12);
+					s = mailbox2.search(/size/);
+					mailbox3 = mailbox2.substring(s + 5);
+					t = mailbox3.search(/size/);
+					mailbox4 = mailbox3.substring(t);
+					u = mailbox4.search(/<B>/);
+					v = mailbox4.search(/<\/B>/);
+					sender = mailbox4.slice(u + 3, v);
+					dump(sender + "\n");
+					w = mailbox4.search(/HREF/);
+					mailbox5 = mailbox4.substring(w);
+					x = mailbox5.search(/>/);
+					y = mailbox5.search(/<\/A>/);
+					subject = mailbox5.slice(x + 1, y);
+					dump(subject + "\n");
+					desc = document.createElement("description");
+					desc.setAttribute("class", "header");
+					desc.setAttribute("value", sender);
+					tipbox.appendChild(desc);
+					desc = document.createElement("description");
+					desc.setAttribute("value", subject);
+					tipbox.appendChild(desc);
+					mailbox = mailbox5;
+				}
+			}
+			else {
+				dump("Error loading page\n");
+			}
+		}
+	};
+	httpReq.send(null);
 }
 
 function ehunShowPopup() {
